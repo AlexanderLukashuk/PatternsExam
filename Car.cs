7aeb1e4b86bc24e5d401e089dcc792f29cb4e791
Саня <sanya@Sergejs-MacBook-Air.local>
2020-12-29 20:32:10 +0600

    Добалвены интерфейсы: Idrive, IRepair, класс Car наследуется от интерфейса IDrive, добавлены классы ServiceStantionRepair, DIYRepair, OfficialRepair, которые наследуются от интерфейса IRepair. В класс Car добавлены методы получения содержимого свойств и модификатор доступа свойств изменен на protected

--- Car.cs
+++ Car.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace PatternsExam
 {
     public interface IDrive
@@ -7,14 +9,62 @@ namespace PatternsExam
 
     public interface IRepair
     {
-        
+        void Repair(Car car);
     }
 
-    public abstract class Car
+    public abstract class ServiceStantionRepair : IRepair
     {
-        public string carBrand;
-        public int price;
-        public string color;
+        public virtual void Repair(Car car)
+        {
+            Console.WriteLine(car.GetName() + "ремонтируется на СТО");
+        }
+    }
 
+    public abstract class DIYRepair : IRepair
+    {
+        public virtual void Repair(Car car)
+        {
+            Console.WriteLine(car.GetName() + "ремонтируется самостоятельно");
+        }
+    }
+
+    public abstract class OfficialRepair : IRepair
+    {
+        public virtual void Repair(Car car)
+        {
+            Console.WriteLine(car.GetName() + "ремонтируется у официалов");
+        }
+    }
+
+    public abstract class Car : IDrive
+    {
+        protected string Name { get; set; }
+        protected string CarBrand { get; set; }
+        protected int Price { get; set; }
+        protected string Color { get; set; }
+        public virtual void Drive()
+        {
+            Console.WriteLine("Машина едет");
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetCarBrand()
+        {
+            return CarBrand;
+        }
+
+        public int GetPrice()
+        {
+            return Price;
+        }
+
+        public string GetColor()
+        {
+            return Color;
+        }
     }
 }
